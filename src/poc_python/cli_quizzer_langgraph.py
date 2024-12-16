5befff4c3b2cf4a05608396c3f504aacf6ce759c
pr0p1k <propiktunes@gmail.com>
2024-12-17 00:45:49 +0100

    Code brush-up

--- src/poc_python/cli_quizzer_langgraph.py
+++ src/poc_python/cli_quizzer_langgraph.py
@@ -1,18 +1,19 @@
-import json
 import os.path
-import time
 from os import listdir
 from os.path import join, isfile, isdir
-from colorama import init as colorama_init
+
 from colorama import Fore
 from colorama import Style
+from colorama import init as colorama_init
 
-from src.poc_python import ROOT_DIR, SAMPLE_INPUTS, BYPASS_CACHE, GENERATED_QUIZZES, JINJA
+from src.poc_python import ROOT_DIR, SAMPLE_INPUTS, BYPASS_CACHE, GENERATED_QUIZZES
 from src.poc_python.process_new_text_langgraph import process_text
 from src.poc_python.ui.cli.quiz_cli import QuizCliUi
+from src.poc_python.utils.output_utils import persist_generated, load_generated
 
 SOURCES_PATH = str(join(ROOT_DIR, SAMPLE_INPUTS))
 GENERATED_QUIZZES_PATH = str(join(ROOT_DIR, GENERATED_QUIZZES, "langgraph"))
+
 
 def get_text_name(file_name: str) -> str:
     return os.path.splitext(file_name)[0].replace("_", " ").title()
@@ -57,18 +58,14 @@ def main():
         text = open(join(ROOT_DIR, SAMPLE_INPUTS, selected_file_name)).read()
         result = process_text(display_name, text)
 
-        with open(join(GENERATED_QUIZZES_PATH, selected_file_name, str(int(time.time())) + ".json"), "w") as output_file:
-            output_file.write(json.dumps(result))
+        persist_generated(GENERATED_QUIZZES_PATH, selected_file_name, result)
     else:
-        # names are <timestamp>.json, str comparison is fine due to slowly changing length of timestamps
-        latest_version = max(listdir(join(GENERATED_QUIZZES_PATH, selected_file_name)))
-
-        with open(join(GENERATED_QUIZZES_PATH, selected_file_name, latest_version)) as json_file:
-            result = json.loads(json_file.read())
+        result = load_generated(GENERATED_QUIZZES_PATH, selected_file_name)
 
     quiz_cli = QuizCliUi.from_state(result)
 
     quiz_cli.start_quiz()
+
 
 if __name__ == "__main__":
     main()
--- src/poc_python/utils/output_utils.py
+++ src/poc_python/utils/output_utils.py
@@ -1,11 +1,15 @@
 import json
+import os.path
 import re
+import time
+from os import makedirs, listdir
+from os.path import join, isdir
 
 from src.poc_python.text_processors.markup import DEFAULT_MARKUP
 
 
 def get_output(response: dict) -> str:
-    if "choices" not in response: # TODO follow some structure
+    if "choices" not in response:  # TODO follow some structure
         return response["output"]
     return response["choices"][0]["text"]
 
@@ -19,7 +23,7 @@ def get_key_points_list_json(json_str: str) -> list[dict]:
 
 
 # TODO choose split logic corresponding to the prompt strategy
-def split_markup_text(text: str, tags = DEFAULT_MARKUP) -> list[tuple]:
+def split_markup_text(text: str, tags=DEFAULT_MARKUP) -> list[tuple]:
     if tags.text_content_start not in text or tags.text_content_end not in text:
         raise Exception("Invalid markup - no start/end tags")
 
@@ -33,10 +37,10 @@ def split_markup_text(text: str, tags = DEFAULT_MARKUP) -> list[tuple]:
     current_index = 0
 
     while True:
-        name_index = text.find(tags.topic_name, current_index) # start of the current topic
+        name_index = text.find(tags.topic_name, current_index)  # start of the current topic
         content_index = text.find(tags.topic_content, current_index + len(tags.topic_name))
 
-        next_name_index = text.find(tags.topic_name, content_index) # end of the current topic
+        next_name_index = text.find(tags.topic_name, content_index)  # end of the current topic
         name = text[name_index + len(tags.topic_name): content_index].strip()
         if next_name_index == -1:
             content = text[content_index + len(tags.topic_content): text.find(tags.text_content_end)]
@@ -53,3 +57,30 @@ def split_markup_text(text: str, tags = DEFAULT_MARKUP) -> list[tuple]:
 
 def split_markup_text_json(json_str: str) -> list[dict]:
     return json.loads(json_str)
+
+
+def persist_generated(base_path: str, source_file_name: str, output: dict):
+    """
+    Save generated quiz to cache in <base_path>/<source_file_name>/<timestamp>.json
+    """
+    destination_dir = join(base_path, source_file_name)
+    if not isdir(destination_dir):
+        makedirs(destination_dir)
+
+    with open(join(destination_dir, str(int(time.time())) + ".json"), "w") as output_file:
+        output_file.write(json.dumps(output))
+
+
+def load_generated(base_path: str, source_file_name: str, version_offset: int = 0) -> dict:
+    """
+    Load previously generated quiz from cache
+    :param base_path: path where the caches are stored
+    :param source_file_name: source file name
+    :param version_offset: if needed to load a non-latest version, offset is taken from the end
+    :return: loaded json object
+    """
+    # names are <timestamp>.json, str comparison is fine due to slowly changing length of timestamps
+    latest_version = max(listdir(join(base_path, source_file_name)))
+
+    with open(join(base_path, source_file_name, latest_version)) as json_file:
+        return json.loads(json_file.read())
